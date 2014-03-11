6b1fb0427dffd86a5965081d652884c497326f1e
Alexander Pronin <pronin.alx@gmail.com>
2014-03-11 21:19:24 +0200

    Initial Commit

---
+++ 2303GesturesTestHW/RITViewController.m
@@ -0,0 +1,29 @@
+//
+//  RITViewController.m
+//  2303GesturesTestHW
+//
+//  Created by Pronin Alexander on 11.03.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import "RITViewController.h"
+
+@interface RITViewController ()
+
+@end
+
+@implementation RITViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
